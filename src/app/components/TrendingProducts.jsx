069845b8531a2a4985f63ded80aec37d8e9c4dab
Nick-Maximillien <nicholasmuthoki@gmail.com>
2025-05-21 21:18:25 +0300

    Update TrendingProducts.jsx

--- src/app/components/TrendingProducts.jsx
+++ src/app/components/TrendingProducts.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import useFetchProducts from '../utils/UseFetchProducts';
+import UseFetchProducts from '../utils/UseFetchProducts';
 import ProductCard from "./ProductCard";
 
 export default async function TrendingProducts() {
