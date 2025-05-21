8b59cf3ede80ebfca753058dee732ad4baa55960
Nick-Maximillien <nicholasmuthoki@gmail.com>
2025-05-21 21:49:59 +0300

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,1 +1,30 @@
+"use server";
+import { UseFetchProducts } from "@utils/useFetchProducts";
+import ProductCard from "../components/ProductCard";
+
+// Define the Product type
+interface Product {
+  id: number;
+  name: string;
+  slug: string;
+  price: number;
+  description: string;
+  rating: string;
+  images?: { image: string }[];
+}
+
+export default async function Products() {
+  const products: Product[] = await UseFetchProducts();
+
+  return (
+    <section className="container py-5 products">
+      <h2 className="text-center fw-bold mb-4">Assorted Products</h2>
+      <div className="row assorted">
+        {products.map((product: Product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
 
